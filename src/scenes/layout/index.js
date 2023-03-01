f184b3eaa6549174190591616e6663be349673dd
jalx1000 <jalejandromogro1@gmail.com>
2023-03-01 14:13:01 -0400

    init Sidebar

--- src/scenes/layout/index.js
+++ src/scenes/layout/index.js
@@ -3,8 +3,10 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
+import { Sidebar } from "components/Sidebar";
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px");
   return (
     <Box width="100%" height="100%">
       <Navbar />
