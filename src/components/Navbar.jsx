3a8d4abbaa15de1ea88f04e32f72372df6b27551
jalx1000 <jalejandromogro1@gmail.com>
2023-03-01 14:08:09 -0400

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,40 @@ const Navbar = () => {
   const theme = useTheme();
   return (
     <AppBar sx={{ position: "static", background: "none", boxShadow: "none" }}>
-      <Toolbar sx={{justifyContent:"space-between"}}></Toolbar>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <FlexBetween>
+          <IconButton onClick={() => console.log("Open/close sidebar")}>
+            <MenuIcon />
+          </IconButton>
+
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0,1rem 1.5rem"
+          >
+            <InputBase placeHolder="Search" />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* RIGHT SIDE */}
+
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
+        </FlexBetween>
+      </Toolbar>
     </AppBar>
   );
 };
