b0963eff5ffeba8565c3d28e0441b292d7cd3eb8
Javier Mogro <jalejandromogro1@gmail.com>
2023-04-09 23:49:30 -0400

    create component product inside of products

--- src/scenes/products/index.jsx
+++ src/scenes/products/index.jsx
@@ -15,9 +15,29 @@ import Header from "components/Header";
 import { useGetProductsQuery } from "state/api";
 
 const Products = () => {
+  const { data, isLoading } = useGetProductsQuery();
+  const isNonMobile = useMediaQuery("( min-width:1000px )");
+  console.log("🚀 ~ file: index.jsx:19 ~ Products ~ data:", data);
   return (
-    <Box>
+    <Box m="1.5rem 2.5rem">
       <Header title="PRODUCTS" subtitle="See your list of products" />
+      {data || isLoading ? (
+        <Box
+          mt="20px"
+          display="grid"
+          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+          justifyContent="space-between"
+          rowGap="20px"
+          columnGap="1.33%"
+          sx={{
+            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+          }}
+        >
+          
+        </Box>
+      ) : (
+        <>Loading</>
+      )}
     </Box>
   );
 };
